35075c44f3012ccc7bce86414135f2871e039b07
rani10245kumari <131008023+rani10245kumari@users.noreply.github.com>
2023-09-09 11:37:10 +0530

    Update Functional.js

--- src/Handson2/Functional.js
+++ src/Handson2/Functional.js
@@ -1,38 +1,41 @@
-import { Component } from "react";
+import React, { useState } from "react";
 
-class Form extends Component {
-    constructor() {
-        super();
-        this.state = {
-            name: "",
-            Department: "",
-            Rating: "",
-            StudentData: [],
-        };
-    }
+function FormCompo() {
+    const [formData, setFormData] = useState({
+        name: "",
+        Department: "",
+        Rating: "",
+    });
 
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-        console.log(e.target.value.length);
+    const [StudentData, setStudentData] = useState([]);
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+        console.log(value.length);
     };
 
-    handleSubmit = () => {
-        const { name, Department, Rating } = this.state;
+    const handleSubmit = () => {
+        const { name, Department, Rating } = formData;
         if (!name || !Department || !Rating) {
             window.alert("Unable to Submit the form");
             return;
         }
 
         const tempObj = {
-            name: this.state.name,
-            Department: this.state.Department,
-            Rating: this.state.Rating,
+            name: formData.name,
+            Department: formData.Department,
+            Rating: formData.Rating,
         };
 
-        const updatedStudentData = [...this.state.StudentData, tempObj];
+        const updatedStudentData = [...StudentData, tempObj];
 
-        this.setState({
-            StudentData: updatedStudentData,
+        setStudentData(updatedStudentData);
+
+        setFormData({
             name: "",
             Department: "",
             Rating: "",
@@ -41,72 +44,70 @@ class Form extends Component {
         console.log(updatedStudentData);
     };
 
-    render() {
-        return (
-            <>
-                <h1 id="heading">EMPLOYEE FEEDBACK FORM</h1>
-                <label className="name" htmlFor="name">
-                    Name:
-                </label>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <input
-                    className="text"
-                    type="text"
-                    id="name"
-                    name="name"
-                    onChange={this.handleChange}
-                    value={this.state.name}
-                />
-                <br />
-                <br />
-                <label className="Department" htmlFor="Department">
-                    Department:
-                </label>
-                &nbsp;&nbsp;&nbsp;
-                <input
-                    className="text"
-                    type="text"
-                    id="Department"
-                    name="Department"
-                    onChange={this.handleChange}
-                    value={this.state.Department}
-                />
-                <br />
-                <br />
-                <label className="Rating" htmlFor="Rating">
-                    Rating:
-                </label>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <input
-                    className="text"
-                    type="number"
-                    id="Rating"
-                    name="Rating"
-                    onChange={this.handleChange}
-                    value={this.state.Rating}
-                />
-                <br />
-                <br />
-                <br />
-                <br />
-                <button className="btn" type="button" onClick={this.handleSubmit}>
-                    Submit
-                </button>
-                <div className="last">
-                    {this.state.StudentData.map((item, index) => {
-                        return (
-                            <div className="last1" key={index}>
-                                <h3>
-                                    Name: {item.name}||Department: {item.Department} || Rating:{" "}
-                                    {item.Rating}{" "}
-                                </h3>
-                            </div>
-                        );
-                    })}
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <h1 id="head">EMPLOYEE FEEDBACK FORM</h1>
+            <label className="name" htmlFor="name">
+                Name:
+            </label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input
+                className="text"
+                type="text"
+                id="name"
+                name="name"
+                onChange={handleChange}
+                value={formData.name}
+            />
+            <br />
+            <br />
+            <label className="Department" htmlFor="Department">
+                Department:
+            </label>
+            &nbsp;&nbsp;&nbsp;
+            <input
+                className="text"
+                type="text"
+                id="Department"
+                name="Department"
+                onChange={handleChange}
+                value={formData.Department}
+            />
+            <br />
+            <br />
+            <label className="Rating" htmlFor="Rating">
+                Rating:
+            </label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input
+                className="text"
+                type="number"
+                id="Rating"
+                name="Rating"
+                onChange={handleChange}
+                value={formData.Rating}
+            />
+            <br />
+            <br />
+            <br />
+            <br />
+            <button className="button" type="button" onClick={handleSubmit}>
+                Submit
+            </button>
+            <div className="end">
+                {StudentData.map((item, index) => {
+                    return (
+                        <div className="end2" key={index}>
+                            <h3>
+                                Name: {item.name}||Department: {item.Department} || Rating:{" "}
+                                {item.Rating}{" "}
+                            </h3>
+                        </div>
+                    );
+                })}
+            </div>
+        </>
+    );
 }
 
-export default Form;
+export default FormCompo;
